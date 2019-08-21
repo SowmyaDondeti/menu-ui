fe2a82734cdf1dff4793ff4cb68ae9c074ade6be
SowmyaDondeti <sowmyar987@gmail.com>
2019-08-20 21:04:36 -0500

    added styling

--- src/Item.js
+++ src/Item.js
@@ -1,19 +1,21 @@
 import React from 'react';
 const itemStyle = {fontFamily: 'Comic Sans MS', fontSize: 15};
-const imageStyle = {width:500, height: 300};
+const imageStyle = {width:200, height: 100, margin:10, border: '2px solid red'};
 
 class Item extends React.Component {
-
-  
 
     render() {
         const{item:{name, description, price, pictureUrl}} = this.props;
       return (
           
-        <div >
-        <li style = {itemStyle} > {name} {description} ${price}</li>
+        <div style={{display: 'flex', flexDirection:'row', alignContent : 'center', width: 450, paddingTop: 10, border: '2px solid #106141'}}>
+        <div style={{display: 'flex', flex:1}}>
         <img style = {imageStyle} src={pictureUrl} alt="new"/>
-        <div/>
+        </div>
+        <div style={{display: 'flex', flex:1, flexDirection:'column',}}>
+        <div style = {itemStyle} > {name} ${price}</div> 
+        <div style = {itemStyle} > {description}</div>
+        </div>
         </div>
       );
     }
